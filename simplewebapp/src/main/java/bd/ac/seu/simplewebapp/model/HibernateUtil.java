097ef345438c02dbda31b153c53abfc2c0648cc5
kmhasan <kmhasan@SEUPC0719>
2017-03-27 12:43:01 +0600

    Simple webapp through JSF

---
+++ simplewebapp/src/main/java/bd/ac/seu/simplewebapp/model/HibernateUtil.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.ac.seu.simplewebapp.model;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+    private static final HibernateUtil INSTANCE = new HibernateUtil();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    private HibernateUtil() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- simplewebapp/src/main/java/bd/ac/seu/simplewebapp/model/Student.java
+++ simplewebapp/src/main/java/bd/ac/seu/simplewebapp/model/Student.java
@@ -5,27 +5,39 @@
  */
 package bd.ac.seu.simplewebapp.model;
 
+import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author kmhasan
  */
 @ManagedBean
+@Entity
 public class Student {
-    private String studentId;
+    @Id
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    private int studentId;
     private String studentName;
 
     public Student() {
+        studentId = 1234;
         studentName = "Md. Hasan Tareque";
     }
 
-    public Student(String studentId, String studentName) {
+    public Student(int studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
     }
 
-    public String getStudentId() {
+    public int getStudentId() {
         return studentId;
     }
 
@@ -35,6 +47,28 @@ public class Student {
 
     public void setStudentName(String studentName) {
         this.studentName = studentName;
+    }
+    
+    public void saveStudent(ActionEvent event) {
+        System.out.println("We are calling the save method");
+        
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Student student = new Student(this.studentId, this.studentName);
+        session.save(student);
+        transaction.commit();
+        session.close();
+    }
+
+    public List<Student> getStudents() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Student> studentsList = session.createCriteria(Student.class).list();
+        session.close();
+        return studentsList;
+    }
+    
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
     
 }
