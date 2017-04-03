496d35707171ce14137afb85d8b8e91694e60599
kmhasan <kmhasan@SEUPC0719>
2017-04-03 11:12:02 +0600

    Contact App

---
+++ contactapp/src/main/java/bd/ac/seu/contactapp/model/Name.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.ac.seu.contactapp.model;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author kmhasan
+ */
+@Embeddable
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name() {
+        firstName = "John";
+        lastName = "Doe";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
